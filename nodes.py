7ead316378b9e6dc7aa85b3885107c175623fdb6
Sam Hodge <102564797+samhodge-aiml@users.noreply.github.com>
2025-03-20 05:18:37 +1030

    Update nodes.py
    update for Python 3

--- nodes.py
+++ nodes.py
@@ -730,7 +730,7 @@ class LivePortraitRetargeting:
         for crop in driving_crop_info["crop_info_list"]:
             if not isinstance(crop, dict):
                 continue
-            if not crop.has_key('lmk_crop'):
+            if 'lmk_crop' not in crop:
                 continue
             driving_landmarks.append(crop['lmk_crop'])
                           
